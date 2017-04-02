e80ef88a9b5ebf8d4fa22b6a177aa557cb2c808e
Rachana Acharya <rasnacharya@gmail.com>
2017-04-02 11:16:53 -0600

    Add few more test to test counting leaves method

--- src/def/tests/BinarySearchTreeTester.java
+++ src/def/tests/BinarySearchTreeTester.java
@@ -5,6 +5,7 @@ import def.BinarySearchTree;
 import def.BSTNode;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * This class def.tests Binary Search Tree
  * Created by Rachana on 4/1/17.
@@ -12,14 +13,14 @@ import static org.junit.jupiter.api.Assertions.*;
 public class BinarySearchTreeTester {
 
     @Test
-    public void testBinaryTreeInsertOneElement(){
+    public void testBinaryTreeInsertOneElement() {
         BinarySearchTree bst = new BinarySearchTree();
         bst.treeInsert(new BSTNode(2));
         assertEquals(1, bst.getBinarySearchTreeSize());
     }
 
     @Test
-    public void testBinaryTreeInsertTwoElement(){
+    public void testBinaryTreeInsertTwoElement() {
         BinarySearchTree bst = new BinarySearchTree();
         BSTNode root = new BSTNode(2);
         BSTNode rightChild = new BSTNode(4);
@@ -30,7 +31,7 @@ public class BinarySearchTreeTester {
     }
 
     @Test
-    public void testBinaryTreeInsertsItemsCorrectly(){
+    public void testBinaryTreeInsertsItemsCorrectly() {
         BinarySearchTree bst = new BinarySearchTree();
         BSTNode root = new BSTNode(12);
         BSTNode item1 = new BSTNode(18);
@@ -48,18 +49,18 @@ public class BinarySearchTreeTester {
         bst.treeInsert(item5);
         bst.treeInsert(item6);
 
-        assertEquals(7,bst.getBinarySearchTreeSize());
+        assertEquals(7, bst.getBinarySearchTreeSize());
         assertEquals(12, bst.getBinarySearchTreeRootNodeKey());
-        assertEquals(2,bst.getLeftChildKey(root));
-        assertEquals(5,bst.getRightChildKey(item3));
-        assertEquals(4,bst.getLeftChildKey(item5));
-        assertEquals(18,bst.getRightChildKey(root));
-        assertEquals(15,bst.getLeftChildKey(item1));
-        assertEquals(17,bst.getRightChildKey(item2));
+        assertEquals(2, bst.getLeftChildKey(root));
+        assertEquals(5, bst.getRightChildKey(item3));
+        assertEquals(4, bst.getLeftChildKey(item5));
+        assertEquals(18, bst.getRightChildKey(root));
+        assertEquals(15, bst.getLeftChildKey(item1));
+        assertEquals(17, bst.getRightChildKey(item2));
     }
 
     @Test
-    public void testGetNumberOfLeaves(){
+    public void testGetNumberOfLeaves() {
         BinarySearchTree bst = new BinarySearchTree();
         BSTNode root = new BSTNode(12);
         BSTNode item1 = new BSTNode(18);
@@ -76,22 +77,56 @@ public class BinarySearchTreeTester {
         bst.treeInsert(item4);
         bst.treeInsert(item5);
         bst.treeInsert(item6);
-        assertEquals(7,bst.getBinarySearchTreeSize());
+        assertEquals(7, bst.getBinarySearchTreeSize());
         assertEquals(12, bst.getBinarySearchTreeRootNodeKey());
-        assertEquals(2,bst.getNumberOfLeaves());
+        assertEquals(2, bst.getNumberOfLeaves());
 
     }
 
     @Test
-    public void testGetNumberOfLeavesOneNode(){
+    public void testGetNumberOfLeavesNullTree() {
+        BinarySearchTree bst = new BinarySearchTree();
+        assertEquals(0, bst.getBinarySearchTreeSize());
+        assertEquals(0, bst.getNumberOfLeaves());
+    }
+
+    @Test
+    public void testGetNumberOfLeavesOneNode() {
         BinarySearchTree bst = new BinarySearchTree();
         BSTNode root = new BSTNode(12);
         bst.treeInsert(root);
-        assertEquals(1,bst.getBinarySearchTreeSize());
+        assertEquals(1, bst.getBinarySearchTreeSize());
         assertEquals(12, bst.getBinarySearchTreeRootNodeKey());
-        assertEquals(1,bst.getNumberOfLeaves());
+        assertEquals(1, bst.getNumberOfLeaves());
+    }
 
+    @Test
+    public void testGetNumberOfLeavesOnBalancedTree() {
+        BinarySearchTree bst = new BinarySearchTree();
+        BSTNode root = new BSTNode(12);
+        BSTNode item1 = new BSTNode(18);
+        BSTNode item2 = new BSTNode(15);
+        BSTNode item3 = new BSTNode(2);
+        BSTNode item4 = new BSTNode(17);
+        BSTNode item5 = new BSTNode(5);
+        BSTNode item6 = new BSTNode(4);
+        BSTNode item7 = new BSTNode(1);
+        BSTNode item8 = new BSTNode(6);
+        BSTNode item9 = new BSTNode(14);
+        BSTNode item10 = new BSTNode(22);
+        bst.treeInsert(root);
+        bst.treeInsert(item1);
+        bst.treeInsert(item2);
+        bst.treeInsert(item3);
+        bst.treeInsert(item4);
+        bst.treeInsert(item5);
+        bst.treeInsert(item6);
+        bst.treeInsert(item7);
+        bst.treeInsert(item8);
+        bst.treeInsert(item9);
+        bst.treeInsert(item10);
 
-
+        assertEquals(11, bst.getBinarySearchTreeSize());
+        assertEquals(6, bst.getNumberOfLeaves());
     }
 }
