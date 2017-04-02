51cf53dfe1c40ebbfb5f42cbbab0351206567de6
Rachana Acharya <rasnacharya@gmail.com>
2017-04-02 00:59:56 -0600

    Add test to test insertions into a binary search tree

--- src/def/tests/BinarySearchTreeTester.java
+++ src/def/tests/BinarySearchTreeTester.java
@@ -26,6 +26,35 @@ public class BinarySearchTreeTester {
         bst.treeInsert(root);
         bst.treeInsert(rightChild);
         assertEquals(2, bst.getBinarySearchTreeSize());
-        assertEquals(4, bst.getRightChild(root));
+        assertEquals(4, bst.getRightChildKey(root));
+    }
+
+    @Test
+    public void testBinaryTreeInsertsItemsCorrectly(){
+        BinarySearchTree bst = new BinarySearchTree();
+        BSTNode root = new BSTNode(12);
+        BSTNode item1 = new BSTNode(18);
+        BSTNode item2 = new BSTNode(15);
+        BSTNode item3 = new BSTNode(2);
+        BSTNode item4 = new BSTNode(17);
+        BSTNode item5 = new BSTNode(5);
+        BSTNode item6 = new BSTNode(4);
+
+        bst.treeInsert(root);
+        bst.treeInsert(item1);
+        bst.treeInsert(item2);
+        bst.treeInsert(item3);
+        bst.treeInsert(item4);
+        bst.treeInsert(item5);
+        bst.treeInsert(item6);
+
+        assertEquals(7,bst.getBinarySearchTreeSize());
+        assertEquals(12, bst.getBinarySearchTreeRootNodeKey());
+        assertEquals(2,bst.getLeftChildKey(root));
+        assertEquals(5,bst.getRightChildKey(item3));
+        assertEquals(4,bst.getLeftChildKey(item5));
+        assertEquals(18,bst.getRightChildKey(root));
+        assertEquals(15,bst.getLeftChildKey(item1));
+        assertEquals(17,bst.getRightChildKey(item2));
     }
 }
